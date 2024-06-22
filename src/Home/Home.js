7d367f4c25d7a1d8233832259137f2db5c28f6c0
Amaira Aggarwal <amairaaggarwal9104@gmail.com>
2024-06-22 16:10:52 +0530

    event updated

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -3,19 +3,19 @@ import Goals from "./Goals";
 import About from "./About";
 import Head from "./Head";
 import Upcoming from "./Upcoming";
-function Home()
-{
-    return(
-        <>
-        <Head/>
-        <h1 className="mainpageheadabout" >About</h1>
-        <About/>
-        <h1 className="mainpagehead" >Goals</h1>
-        <Goals/>
-        <h1 className="mainpagehead" id="event">Recent Events</h1>
-        <Upcoming/>
-
-        </>
-    )
+function Home() {
+  return (
+    <>
+      <Head />
+      <h1 className="mainpageheadabout">About</h1>
+      <About />
+      <h1 className="mainpagehead">Goals</h1>
+      <Goals />
+      <h1 className="mainpagehead" id="event">
+        Upcoming Events
+      </h1>
+      <Upcoming />
+    </>
+  );
 }
 export default Home;
